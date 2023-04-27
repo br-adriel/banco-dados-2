d25bf2799443e4d4c9550502ee37a638557e8ae8
Adriel <adriel.fariasan@gmail.com>
2023-04-27 14:14:54 -0300

    Adapta tarefa01-creates para postgresql #1

--- tarefas/t01/tarefa01-create.sql
+++ tarefas/t01/tarefa01-create.sql
@@ -1,52 +1,46 @@
-SET FOREIGN_KEY_CHECKS=0; -- Desabilitar Foreign Keys
-
 drop table if exists atividade cascade;
 drop table if exists projeto cascade;
 drop table if exists departamento cascade;
 drop table if exists funcionario cascade;
 
-SET FOREIGN_KEY_CHECKS=1; -- Habilitando Foreign Keys
-
 # Criação das Tabelas
 
 CREATE TABLE funcionario (
-	codigo int AUTO_INCREMENT,
+	codigo serial primary key,
 	nome varchar(50),
 	sexo char(1),
 	dtNasc date,
 	salario decimal(10,2),
 	codSupervisor int,
 	codDepto int,
-	PRIMARY KEY (codigo),
+  constraint fk_supervisor
 	FOREIGN KEY (codSupervisor) REFERENCES funcionario(codigo) on delete set null on update cascade
 );
 
 CREATE TABLE departamento (
-	codigo int AUTO_INCREMENT,
-	sigla varchar(10),
+	codigo serial,
+	sigla varchar(10) unique,
 	descricao varchar(50),
 	codGerente int,
 	PRIMARY KEY (codigo),
-	UNIQUE KEY depSigla (sigla),
 	FOREIGN KEY (codGerente) REFERENCES funcionario(codigo) on delete set null on update cascade
 );
 
 CREATE TABLE projeto (
-	codigo int AUTO_INCREMENT,
-	nome varchar(50),
+	codigo serial,
+	nome varchar(50) unique,
 	descricao varchar(250),
 	codResponsavel int,
 	codDepto int,
 	dataInicio date, 
 	dataFim date,
 	PRIMARY KEY (codigo),
-	UNIQUE KEY projNome (nome),
 	FOREIGN KEY (codResponsavel) REFERENCES funcionario(codigo) on delete set null on update cascade,
 	FOREIGN KEY (codDepto) REFERENCES departamento(codigo) on delete set null on update cascade
 );
  
 CREATE TABLE atividade (
-	codigo int AUTO_INCREMENT,
+	codigo serial,
 	descricao varchar(250),
 	codProjeto int,
 	dataInicio date, 
