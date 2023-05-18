82c3b2fa0bcb50fd1c1624c33e322976f4dca447
Adriel <adriel.fsantos@outlook.com>
2023-05-18 13:39:30 -0300

    Gera migração de criação da tabela departamento #4

---
+++ tarefas/t03/questao02/orm/migrations/20230518162201-create-departamento.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('departamentos', {
+      codigo: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      sigla: {
+        type: Sequelize.STRING(15),
+        allowNull: false,
+        unique: true,
+      },
+      descricao: {
+        type: Sequelize.STRING(25),
+        allowNull: false,
+      },
+      gerente: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'funcionario',
+          key: 'codigo',
+        },
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('departamentos');
+  },
+};
